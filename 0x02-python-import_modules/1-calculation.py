cefac5f4ea34fe31448878e498f63687a6c405f1
Emmanuel Onyekachi <emmanuelonyekachi04122000@gmail.com>
2023-12-04 18:10:48 +0000

    Task 1

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import calculator_1 as cal
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+
+    plus = cal.add(a, b)
+    print("{} + {} = {}".format(a, b, plus))
+    print("{} - {} = {}".format(a, b, cal.sub(a, b)))
+    print("{} * {} = {}".format(a, b, cal.mul(a, b)))
+    print("{} / {} = {}".format(a, b, cal.div(a, b)))
