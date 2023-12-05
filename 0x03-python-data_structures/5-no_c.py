e964b232d98e1d5e0bb7dce6676df6cea94a0d2f
Emmanuel Onyekachi <emmanuelonyekachi04122000@gmail.com>
2023-12-05 18:23:59 +0000

    Changed function body

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
     new_str = ""
+
     for char in my_string:
-        if ord(char) == ord('c') or ord(char) == ord('C'):
-            continue
-        new_str += char
+        if char != 'c' and char != 'C':
+            new_str += char
     return new_str
