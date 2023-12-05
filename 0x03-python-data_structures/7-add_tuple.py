d67b93e3cb614b0713ddd8c5085be9f67e55fb98
Emmanuel Onyekachi <emmanuelonyekachi04122000@gmail.com>
2023-12-05 21:21:25 +0000

    Task 7

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    sum_tuple = ()
+
+    if len(tuple_a) < 2:
+        tuple_a = tuple_a + (0,) * (2 - len(tuple_a))
+    elif len(tuple_b) < 2:
+        tuple_b = tuple_b + (0,) * (2 - len(tuple_b))
+
+    first = tuple_a[0] + tuple_b[0]
+    second = tuple_a[1] + tuple_b[1]
+
+    return first, second
