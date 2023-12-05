f8307c9bc9fb43582d6ae5755c3e139199ec41c6
Emmanuel Onyekachi <emmanuelonyekachi04122000@gmail.com>
2023-12-05 04:04:25 +0000

    Task 4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    new_list = my_list.copy()
+
+    length = len(my_list)
+
+    if idx < 0 or idx >= length:
+        return my_list
+
+    else:
+        new_list[idx] = element
+        return new_list
