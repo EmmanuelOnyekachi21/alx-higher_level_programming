247ce2613a2c73e7a70b474bce4e28192b67520f
Emmanuel Onyekachi <emmanuelonyekachi04122000@gmail.com>
2023-12-05 00:48:03 +0000

    Task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,9 +2,7 @@
 import hidden_4     # Import the compiled module
 
 if __name__ == "__main__":
-    module_names = dir(hidden_4)    # Get all names defined in the compiled module
-
-    # Print names that do not start with '__'
-    for name in sorted(module_names):
-        if not name.startswith('__'):
-            print(name)
+    for value in dir(hidden_4):
+        if value[:2] == "__":
+            continue
+        print(value)
