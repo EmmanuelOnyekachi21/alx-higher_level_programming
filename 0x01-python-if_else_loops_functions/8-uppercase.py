f36c0b8f26103fda5a92d2f13e6ff4ab70336fdb
Emmanuel Onyekachi <emmanuelonyekachi04122000@gmail.com>
2023-12-06 06:11:52 +0000

    Task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,7 +4,7 @@ def uppercase(str):
     result = ""
 
     for i in range(len(str)):
-        if ord(str[i]) >= 97 and ord(str[i]) <=122:
+        if ord(str[i]) >= 97 and ord(str[i]) <= 122:
             result += chr(ord(str[i]) - 32)
         else:
             result += str[i]
