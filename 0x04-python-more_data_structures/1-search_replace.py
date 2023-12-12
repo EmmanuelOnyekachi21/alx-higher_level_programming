d6bcb9a58151bae8f86fe06196713c68af0a9fbf
Demon <emmanuelonyekachi04122000@gmail.com>
2023-12-12 02:52:57 +0000

    Task 1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    if my_list is None or len(my_list) == 0:
+        return my_list
+
+    new_m = []
+    for x in my_list:
+        if x == search:
+            new_m.append(replace)
+            continue
+        new_m.append(x)
+    return new_m
+# Using list comprehension
+"""
+new_list = [x if x != search else replace for x in my_list]
+"""
