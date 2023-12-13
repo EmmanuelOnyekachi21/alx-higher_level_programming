b4987ea0718c3c0a1552d37ac49bac8e74f39797
Demon <emmanuelonyekachi04122000@gmail.com>
2023-12-13 03:56:47 +0000

    Task 4

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -3,4 +3,5 @@ def only_diff_elements(set_1, set_2):
     return set_1 ^ set_2
 
 # Using List comprehension
-#    uncommon_set = {element for element in set_1 if element not in set_2} | {element for element in set_2 if element not in set_1}
+#    uncommon_set = {element for element in set_1 if element not in set_2} | 
+# {element for element in set_2 if element not in set_1}
