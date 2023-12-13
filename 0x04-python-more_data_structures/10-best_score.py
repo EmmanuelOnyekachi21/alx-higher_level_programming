d0921abe6ff9517d7bfc292ca1207ed85e365095
Demon <emmanuelonyekachi04122000@gmail.com>
2023-12-13 05:02:01 +0000

    Task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -6,7 +6,7 @@ def best_score(a_dictionary):
     max_score = max(a_dictionary.values())
 
     for key, value in a_dictionary.items():
-        if value == max_score:      
+        if value == max_score:
             return key
 
     return None     # Return None if no score is found
