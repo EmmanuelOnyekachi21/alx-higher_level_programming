9a136f68959799b51974ee4488ae3b86c45cad1e
Demon <emmanuelonyekachi04122000@gmail.com>
2024-01-05 02:06:12 +0000

    Task 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" This is a rectangle module"""
+
+
+class Rectangle:
+    """
+    This class defines a Rectangle.
+
+    Attributes:
+        No attributes are defined
+
+    Methods:
+        No methods are defined for an empty Rectangle instance.
+        """
+    pass
