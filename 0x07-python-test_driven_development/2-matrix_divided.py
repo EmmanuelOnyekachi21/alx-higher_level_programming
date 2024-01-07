240257b435d7045492f6eac7f21c6be079f6d8c8
Demon <emmanuelonyekachi04122000@gmail.com>
2024-01-07 01:46:48 +0000

    Added Task 1 with its correspending test doc

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""This is a module for matrix division"""
+
+
+def matrix_divided(matrix, div):
+    for row in matrix:
+        if not isinstance(row, list):
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    for row in matrix:
+        if not all(isinstance(item, (int, float)) for item in row):
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    num_cols = len(matrix[0])
+    for rows in matrix:
+        if len(rows) != num_cols:
+            raise TypeError("Each row of the matrix must have the same size")
+
+    new_matrix = []
+
+    if (div == 0):
+        raise ZeroDivisionError("division by zero")
+
+    if not isinstance(div, (float, int)):
+        raise TypeError("div must be a number")
+
+    for rows in matrix:
+        new_row = []
+        for cols in rows:
+            result = round(cols / div, 2)
+            new_row.append(result)
+        new_matrix.append(new_row)
+
+    return new_matrix
