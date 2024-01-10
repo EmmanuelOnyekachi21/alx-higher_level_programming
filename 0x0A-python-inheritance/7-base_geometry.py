f44f210451656d6aff73aa95fcaa164444fc6c67
Demon <emmanuelonyekachi04122000@gmail.com>
2024-01-10 17:08:11 +0000

    Added files related to Task 7

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -34,7 +34,7 @@ class BaseGeometry:
             TypeError: If value is not an integer
             ValueError: If value is less than or equal to 0
         """
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError(f"{name} must be an integer")
 
         if value <= 0:
