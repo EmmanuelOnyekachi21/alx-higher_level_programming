5332389edc48ea206b0da84ff83c36c99d0548b9
Demon <emmanuelonyekachi04122000@gmail.com>
2024-01-10 11:01:42 +0000

    Task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This module checks if object is an instance or an instance inherited from a
+specified class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    checks if object is an instance or an instance inherited from a
+    specified class
+    
+    Args:
+        obj: Object
+        a_class: A class
+
+    Returns:
+        bool: Either true or false
+    """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
