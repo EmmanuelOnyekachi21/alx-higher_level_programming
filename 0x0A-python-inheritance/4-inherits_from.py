19c91d717ae38836e2558db80ffebc959c1ad139
Demon <emmanuelonyekachi04122000@gmail.com>
2024-01-10 13:51:55 +0000

    Task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This module returns a True or False depending on if the
+object is an instance of a class that 
+inherited (directly or indirectly) from the specified class
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Checks if the given object is an instance of a class that inherited
+    from the specified class.
+
+    Args:
+        obj: An object to check for inheritance.
+        a_class: The class to check for inheritance from.
+
+    Returns:
+        bool: True if the object is an instance of a class inherited 
+                from the specified class, otherwise False.
+    """
+    if isinstance(obj, type):
+        if issubclass(type(obj), a_class) and type(obj) != a_class:
+            return True
+    return False
