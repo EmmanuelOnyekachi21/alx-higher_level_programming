e4ae731103a3f72d0a6b893bb2195810a75719d5
Demon <emmanuelonyekachi04122000@gmail.com>
2024-01-12 01:44:28 +0000

    Task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import json
+"""3. To JSON string """
+
+
+def to_json_string(my_obj):
+    """
+    This function returns the JSON representation of an object (string)
+
+    Args:
+        obj: An object(string)
+
+    Returns:
+        json_written: The JSON representation of `my_obj`
+    """
+
+    json_written = json.dumps(my_obj)
+
+    return json_written
