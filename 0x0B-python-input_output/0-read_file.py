228eec1098bf3292cb22b601b56b9154d2c51292
Demon <emmanuelonyekachi04122000@gmail.com>
2024-01-12 01:09:54 +0000

    TAsk 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
+"""This module pprovides functions for reading files"""
+
 
 def read_file(filename=""):
+    """
+    Read the content of a text_file and print it to stdout.
+
+    Args:
+        filename (str): The name of the file to read.
+
+    Returns:
+        None.
+    """
+
     with open(filename, "r", encoding="UTF-8") as f:
         for lines in f:
             print(lines.strip())
