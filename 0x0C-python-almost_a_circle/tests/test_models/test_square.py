236bc5f35494ae2e0d8695e85aa0b7940007bc05
Demon <emmanuelonyekachi04122000@gmail.com>
2024-01-19 19:38:15 +0000

    Added its test file

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -23,6 +23,23 @@ class TestSquare(unittest.TestCase):
         self.assertTrue(isinstance(s1, Square))
         self.assertTrue(isinstance(s1, Rectangle))
 
+    def test_square_size_getter(self):
+        s1 = Square(5)
+        self.assertEqual(s1.size, 5)
+
+    def test_square_size_setter(self):
+        s1 = Square(5)
+        s1.size = 10
+        self.assertEqual(s1.size, 10)
+        self.assertEqual(s1.width, 10)
+        self.assertEqual(s1.height, 10)
+
+    def test_square_setter_invalid(self):
+        s1 = Square(5)
+
+        with self.assertRaises(TypeError):
+            s1.size = "9"
+
 
 if __name__ == "__main__":
     unittest.main()
