7e2b924bab6fc20ba6ca1b36a58d140fd7d133ef
Demon <emmanuelonyekachi04122000@gmail.com>
2024-03-03 15:38:17 +0000

    Task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 from add_0 import add
 
-if __name__ == "__main__":
-    a = 1
-    b = 2
-    result = add(a, b)
-
-    print("{} + {} = {}".format(a, b, result))
+result = add(1, 2)
+print("1 + 2 = {}".format(result))
