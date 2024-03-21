dd9d7bb0f3748713aa119791abf43b146dd570b4
Demon <emmanuelonyekachi04122000@gmail.com>
2024-03-21 02:18:41 +0000

    Advanced Task 1

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 """This module has a class `MyInt` inheriting from class `int`
 """
+
+
 class MyInt(int):
     """MyInt class inherits from int but inverts the behavior of == and !=."""
     def __eq__(self, other):
-        """Overrides the == operator to return the opposite of the original comparison."""
+        """Overrides the == operator to return the opposite of the
+        original comparison."""
         return not super().__eq__(other)
+
     def __ne__(self, other):
-        """Overrides the != opertor to return the opposite of the original comparison."""
+        """Overrides the != opertor to return the opposite of the
+        original comparison."""
         return not super().__ne__(other)
