cab9641dd8a50c98ab41d0adec56f76769bc9223
EmmanuelOnyekachi21 <emmanuelonyekachi04122000@gmail.com>
2024-04-16 17:53:39 -0400

    Fixed requirement in Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Document"""
+"""Document modules"""
 import json
 import sys
-
 save = __import__('5-save_to_json_file').save_to_json_file
 load = __import__('6-load_from_json_file').load_from_json_file
+
 
 filename = 'add_item.json'
 items = load(filename)
