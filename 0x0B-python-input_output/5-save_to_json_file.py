2691fbcd19c65308a75430b73a88c83fd5849501
EmmanuelOnyekachi21 <emmanuelonyekachi04122000@gmail.com>
2024-04-16 10:49:51 -0400

    Task 5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,5 +1,8 @@
+#!/usr/bin/python3
+"""Document"""
 import json
 
 def save_to_json_file(my_obj, filename):
+    """Document"""
     with open(filename, "w") as file:
         json.dump(my_obj, file)
