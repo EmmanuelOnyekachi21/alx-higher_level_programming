e35118eb9b48e66333b4767207dac27eacdc2ca9
EmmanuelOnyekachi21 <emmanuelonyekachi04122000@gmail.com>
2024-04-18 11:08:53 -0400

    Dealt with pycodestyle yappings.

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,7 +3,8 @@
 This module defines the Base class.
 
 The Base class serves as the base class for all other classes in this project.
-It manages the 'id' attribute for all future classes and avoids duplicating code.
+It manages the 'id' attribute for all future classes,
+and avoids duplicating code.
 """
 
 
@@ -13,7 +14,7 @@ class Base:
 
     Attribute(s):
         __nb_objects: private class attibute; initialized to 0.
-        id (int): A public instance attribute representing the id of the object.
+        id (int): A public instance attribute; reps the id of the object.
     Method(s):
         __init__(self, id=None): The method that initializes the Base class.
     """
@@ -23,7 +24,8 @@ class Base:
         """Initializes a Base object.
 
         Args:
-            id (int, optional): The id of the object.  If None, it is auto-generated.
+            id (int, optional): The id of the object.
+                                If None, it is auto-generated.
         Returns:
             None.
         """
