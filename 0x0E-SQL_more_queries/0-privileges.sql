dffde6f6544a34ff718cd4fdf57dd644ec29616f
Emmanuel <emmanuelonyekachi04122000@gmail.com>
2024-05-29 00:37:41 +0100

    Task 0

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,7 @@
+-- a script that lists all privileges of the MySQL users user_0d_1 and user_0d_2
+
+-- List priviledges for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- List priviledges for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
