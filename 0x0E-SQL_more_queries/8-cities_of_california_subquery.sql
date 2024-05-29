a330857435a098fd98c4db1a5fa29c968e307c68
Emmanuel <emmanuelonyekachi04122000@gmail.com>
2024-05-29 01:48:41 +0100

    Task 8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- Select cities in California sorted by cities.id
-SELECT * FROM cities
+SELECT id, name FROM cities
 WHERE state_id = (SELECT id FROM states WHERE name = 'California')
 ORDER BY id ASC;
