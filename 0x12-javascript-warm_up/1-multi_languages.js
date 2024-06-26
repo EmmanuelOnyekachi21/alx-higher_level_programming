73ad7568ac853583f9296253d4b8693b638c214f
Emmanuel <emmanuelonyekachi04122000@gmail.com>
2024-06-26 15:46:46 +0100

    Task 1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/**
+ * Script prints the 3 lines below:
+ * The first line: “C is fun”
+ * The second line: “Python is cool”
+ * The third line: “JavaScript is amazing”
+ */
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
