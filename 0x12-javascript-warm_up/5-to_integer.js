1f738a039dd7388bd36f49f9c128a406f3867269
Emmanuel <emmanuelonyekachi04122000@gmail.com>
2024-06-27 04:02:11 +0100

    Task 5

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/* prints My number: <first argument converted in integer> if the first argument can be converted to an integer: */
+const args = process.argv.slice(2);
+// parseInt requires the string to be converted to int and 10, i.e decimal
+// when convert to binary, 2 = binary
+const number = parseInt(args[0], 10);
+if (!(isNaN(number))) {
+  console.log('My number: ' + args[0]);
+} else {
+  console.error('Not a number');
+}
