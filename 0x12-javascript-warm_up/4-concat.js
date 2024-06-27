c21c06547a2ed120e6f63eadadc2721e8e075e5d
Emmanuel <emmanuelonyekachi04122000@gmail.com>
2024-06-27 03:33:26 +0100

    Task 4 - Declared variables with const

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -2,6 +2,6 @@
 /*
  * prints two arguments passed to it, in the following format: “ is ”
  */
-isString = ' is ';
-args = process.argv.slice(2);
+const isString = ' is ';
+const args = process.argv.slice(2);
 console.log(args[0] + isString + args[1]);
