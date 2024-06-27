603fa9e7656a2532ca6572101722720fa7e8f534
Emmanuel <emmanuelonyekachi04122000@gmail.com>
2024-06-27 03:14:30 +0100

    Added comment

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 /* Prints a message depending on the number of arguments passed. */
-const args = process.argv;
+const args = process.argv; // FIrst two argument in process.argv is #!/usr/bin/node and name of file
 if (args.length <= 2) {
   console.log('No argument');
 } else if (args.length === 3) {
