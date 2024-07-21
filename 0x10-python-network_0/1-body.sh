c6fae20fbe9f02df1bc7cc58c48d593bdd397fd1
Emmanuel <emmanuelonyekachi04122000@gmail.com>
2024-07-21 22:57:00 +0100

    Task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a GET request to a URL and displays the body of the response if the status is 200
-curl -s -o response.txt -w "%{http_code}" "$1" | grep -q "^200$" && cat response.txt
+curl -sL -o response.txt -w "%{http_code}" "$1" | grep -q "^200$" && cat response.txt
