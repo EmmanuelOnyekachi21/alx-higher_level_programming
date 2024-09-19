2dd8d378d0ecb26a193bedebb414fb503dcfff69
Emmanuel Onyekachi <emmanuelonyekachi04122000@gmail.com>
2024-09-19 03:53:39 +0100

    Task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,56 +1,29 @@
 #!/usr/bin/python3
-
-"""
-This script takes in the name of a state as an argument and lists all cities
-of that state, using the database hbtn_0e_4_usa.
-
-It takes 4 arguments: mysql username, mysql password, database name and
-state name (SQL injection free).
+""" This is a script that uses MySQLdb to lists all cities available for a
+given state which is passed as an argument, using the database hbtn_0e_4_usa
+and provides protection against SQL injection attacks.
 """
 
-
-import MySQLdb
 import sys
+import MySQLdb
 
-if __name__ == '__main__':
-    if len(sys.argv) != 4:
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
         exit()
-
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-    state_name = sys.argv[4]
-
-    # Establish a connection to the database
-    db = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=mysql_username,
-        passwd=mysql_password,
-        db=database_name
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    query = (
+        "SELECT cities.name "
+        "FROM cities "
+        "JOIN states ON cities.state_id = states.id "
+        "WHERE states.name = %s "
+        "ORDER BY cities.id ASC"
     )
-
-    cursor = db.cursor()
-
-    query = """
-    SELECT name
-    FROM cities
-    WHERE states.id = (SELECT id FROM states WHERE name = %s)
-    ORDER BY id ASC
-    """
-    cursor.execute(query, (state_name,))
-
-    # Fetch all the results of the query
-    cities = cursor.fetchall()
-
-    for num, name in enumerate(cities):
-        values = name[0]
-        if num == (len(cities) - 1):
-            print(value, end='')
-        else:
-            print(value, end=', ')
-    print()
-
-    # Close the cursor and the database connection
-    cursor.close()
+    cur.execute(query, (sys.argv[4],))
+    cities = cur.fetchall()
+    
+    print(", ".join([city[0] for city in cities]))
+    cur.close()
     db.close()
